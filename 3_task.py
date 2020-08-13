e83109e5f5c7eeab7e83ed49e04f9cb153c33308
YuryKorolyov <66995765+YuryKorolyov@users.noreply.github.com>
2020-08-13 12:35:15 +0300

    Update 3_task.py

--- 3_task.py
+++ 3_task.py
@@ -1,17 +1,39 @@
 #Простые делители числа 13195 - это 5, 7, 13 и 29.
 
 #Каков самый большой делитель числа 600851475143, являющийся простым числом?
-
+import math
 number = 600851475143
 
-def is_prime(x):
-    for i in range(2, x): # это промежуток между 1 и x, если % больше 0 на каждой итерации - число простое.
-        if x % i == 0:
-            return False
-    return True
+# Получаем делители числа
 
+def get_factors(number):
+    factors = []
+    for i in range(1, int(math.sqrt(number))+1):
+        if number % i == 0:
+            factors.append(i)
+            factors.append(number // i)
+    return factors
 
-for a in range(1, 600851475143+1):
-    if is_prime(a) and number % a == 0:
-        print(a)
-    
+print(get_factors(10))
+
+# Определяем, является ли делитель простым числом
+
+def is_prime(number):
+    if len(get_factors(number)) == 2 :
+        return True
+    else:
+        return False
+
+print(is_prime(1))
+
+# Находим наибольший простой делитель
+
+all_factors = get_factors(number)
+print(all_factors)
+largest_prime_factor = 0
+
+for factor in all_factors:
+   if is_prime(factor) and factor > largest_prime_factor:
+       largest_prime_factor = factor
+
+print(largest_prime_factor)
